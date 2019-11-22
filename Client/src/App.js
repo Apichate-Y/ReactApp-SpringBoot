d874d8a1c296e05c85c2a62630814a48a72b48da
GlomGlom <apichate.yatra@gmail.com>
2019-11-22 08:43:24 +0700

    edit ui

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import GroupList from './GroupList';
+import GroupEdit from './GroupEdit';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route path='/' exact={true} component={Home}/>
+          <Route path='/groups' exact={true} component={GroupList}/>
+          <Route path='/groups/:id' component={GroupEdit}/>
+        </Switch>
+      </Router>
+    )
+  }
 }
 
 export default App;
